b3b9b6b56781341d427f0c03b4fcb72e27808880
Stanislav Cinkl <stanislav.cinkl@student.spsmb.cz>
2023-10-31 11:59:18 +0100

    hofhgif

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,14 @@
 
 
 const ham = document.getElementById("bellingham");
+const ron = document.getElementById("roncek");
 
 let goal = new Audio('./res/img/gol.mp3')
+let sui = new Audio('./res/img/suiii.mp3')
 
 ham.onclick = () => {
     goal.play()
+}
+ron.onclick = () => {
+    sui.play()
 }
